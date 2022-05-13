66935fa27e3d5185e319ec9554a4aa4f047a3937
Guilherme Emanoel França Monteiro <guiemanoel139@gmail.com>
2022-05-12 22:41:12 -0300

    aula funcoes 12/05/2022

---
+++ modulo1/operadores/template_javascript/11-05.js
@@ -0,0 +1,82 @@
+
+
+
+// const nome = prompt("Qual é o seu nome?")
+// const corFavorita = prompt("Qual é sua cor favorita?")
+
+// console.log("A cor favorita de " + nome + " é" + corFavorita)
+
+// console.log(`A cor favorita de ${nome} é ${corFavorita}`)
+
+// const estudante = "Guilherme"
+
+// const comprimento = estudante.length
+
+// console.log(comprimento)
+
+// const frase = "BUCETAO"
+
+// const fraseMinusculo = frase.toLowerCase()
+
+// console.log(fraseMinusculo)
+
+
+// const email = "      guiemanoel139@gmail.com       "
+
+// const emailSemEspaço = email.trim()
+
+// console.log(emailSemEspaço)
+
+// const estudante = "Guilherme Emanoel"
+
+// console.log(estudante.includes("uilg"))
+
+// const vogais = "aaaaaaeeeeeiiiooouuu"
+
+// const vogaisAlteradas = vogais.replaceAll("a", "i")
+
+// console.log(vogais)
+// console.log(vogaisAlteradas)
+
+
+// const frase = prompt("Insira uma frase aqui")
+
+// const minuscula = frase.toLowerCase()
+// console.log(minuscula)
+
+// const vogais = frase.replaceAll("o", "i")
+// console.log(vogais)
+
+// const tamanhoFrase = frase.length
+// console.log(tamanhoFrase)
+
+// const listaDeCompra = ["Feijão", "Arroz", "Farinha"]
+// const item1 =  listaDeCompra[0]
+// console.log(item1)
+
+// const animais = ["Aranha", "Cachorro", "Gato", "Macaco", "Cobra"]
+// console.log (animais.length)
+// const numero = prompt("Insira um numero de 1 a 5 aqui")
+
+// console.log(numero)
+
+// const item1 = animais[numero]
+
+// console.log(item1)
+
+
+const numeros = [1, 2, 3, 4, 5, 6]
+
+const tamanho = numeros.length
+console.log(tamanho)
+
+const numero7= numeros.push(7)
+console.log(numero7)
+
+const remocao45 = numeros.splice(3,2)
+console.log(remocao45)
+
+const tamanhoFinal = numeros.length
+
+
+
