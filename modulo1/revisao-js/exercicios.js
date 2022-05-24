e8ea5dd563adfc92499a0138e520047fd0ff54ee
Guilherme Emanoel França Monteiro <guiemanoel139@gmail.com>
2022-05-23 21:14:17 -0300

    revisao js 23/05/2022

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,109 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   return array.length
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  return array.reverse()
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+   return array.sort(ordenacao)
+
+    function ordenacao (a, b) {
+      return a - b 
+  }
+
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  return array.filter % 2 === 0
+  
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ let numeroPar = array [0]
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+   let maiorNumero = array [0]
+    for (let i = 1; i < array.length; i++) {
+        if (array [i] > maiorNumero){
+            maiorNumero = array[i]
+        }
+    }
+    return maiorNumero
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+   
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  // entrada
+
+}
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+    // {
+    //     nome: 'O Diabo Veste Prada',
+    //     ano: 2006,
+    //     diretor: 'David Frankel',
+    //     atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+    //  }
+     
+    // return console.log (`Venha assistir ao filme ${nome}, de ${ano}, dirigido por ${diretor} e estrelado por ${atores[0]}, ${atores[1]}, ${atores[2]}, ${atores[3]}.`)
+    // }
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// // EXERCÍCIO 15B
+// function retornaArrayOrdenadoPorData(consultas) {}
